a969d2c1b7fe05a04be410d6ebdfdb1574730e67
RAVALIKA <ravalika09@gmail.com>
2026-04-29 03:25:00 +0000

    diff-multi-Ravalika

--- Myapp.java
+++ Myapp.java
@@ -2,4 +2,6 @@ public class Message{
 	public static void main(String[] args){
 		int a=10, int b=20;
 		System.out.println("Sum is:",+(a+b));
+		System.out.println("Difference is:",+(a-b));
+		System.out.println("Multiply is:",+(a*b));
 	}
